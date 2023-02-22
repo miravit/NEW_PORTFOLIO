b083ff925dc01a401ef7c188ece4518823bfaead
Mira <miravitsas1@gmail.com>
2023-02-22 15:23:18 +0100

    fix

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -40,12 +40,15 @@ todoPhoto.addEventListener("click", () => {
       "todo-mobile"
     ) as HTMLDivElement;
     const todoHead: HTMLParagraphElement = document.createElement("p");
+    const todoKey: HTMLParagraphElement = document.createElement("p");
     const todoText: HTMLSpanElement = document.createElement("span");
     const todoLink: HTMLAnchorElement = document.createElement("a");
     todoHead.classList.add("clickedHead");
+    todoKey.classList.add("clickedKey");
     todoText.classList.add("clickedText");
     todoLink.classList.add("clickedLink");
     todoHead.innerHTML = "School";
+    todoKey.innerHTML = "Javascript, HTML, CSS";
     todoText.innerHTML =
       "My To-Do List web application allows users to quickly and easily create and manage tasks. Built with JavaScript, HTML and CSS, the app utilizes local storage to save user data. With a simple design, users can add and delete tasks with a single click. Additionally, users can mark tasks as completed, which will be stored and displayed in a separate list.";
     todoLink.innerHTML = "See Project";
@@ -58,6 +61,7 @@ todoPhoto.addEventListener("click", () => {
     //   todoMobile.setAttribute("src", "./assets/todolist-mobile.png");
     todoDiv.appendChild(todoHead);
     todoDiv.appendChild(todoText);
+    todoDiv.appendChild(todoKey);
     todoDiv.appendChild(todoLink);
   }
   todo = false;
@@ -73,12 +77,15 @@ tshirtPhoto.addEventListener("click", () => {
       "tshirt-mobile"
     ) as HTMLDivElement;
     const tshirtHead: HTMLParagraphElement = document.createElement("p");
+    const tshirtKey: HTMLParagraphElement = document.createElement("p");
     const tshirtText: HTMLSpanElement = document.createElement("span");
     const tshirtLink: HTMLAnchorElement = document.createElement("a");
     tshirtHead.classList.add("clickedHead");
+    tshirtKey.classList.add("clickedKey");
     tshirtText.classList.add("clickedText");
     tshirtLink.classList.add("clickedLink");
     tshirtHead.innerHTML = "School";
+    tshirtKey.innerHTML = "Javascript, HTML, CSS";
     tshirtText.innerHTML =
       "This group project is a simulated web shop application built with JavaScript and uses local storage to store data. It allows users to shop and add products to their cart, adjust the quantity and size of their items, and simulate a purchase. Customers can also view their cart and checkout when they are ready to make their purchase. The application is intuitive and user-friendly, making it easy for shoppers to find and purchase the products they need.";
     tshirtLink.innerHTML = "See Project";
@@ -92,6 +99,7 @@ tshirtPhoto.addEventListener("click", () => {
     //   todoMobile.setAttribute("src", "./assets/todolist-mobile.png");
     tshirtDiv.appendChild(tshirtHead);
     tshirtDiv.appendChild(tshirtText);
+    tshirtDiv.appendChild(tshirtKey);
     tshirtDiv.appendChild(tshirtLink);
   }
   tshirt = false;
@@ -107,13 +115,17 @@ recreatePhoto.addEventListener("click", () => {
       "recreate-mobile"
     ) as HTMLDivElement;
     const recreateHead: HTMLParagraphElement = document.createElement("p");
+    const recreateKey: HTMLParagraphElement = document.createElement("p");
     const recreateText: HTMLSpanElement = document.createElement("span");
     const recreateLink: HTMLAnchorElement = document.createElement("a");
     recreateHead.classList.add("clickedHead");
+    recreateKey.classList.add("clickedKey");
     recreateText.classList.add("clickedText");
     recreateLink.classList.add("clickedLink");
     recreateHead.innerHTML = "School";
-    recreateText.innerHTML = " recreate";
+    recreateKey.innerHTML = "HTML, CSS";
+    recreateText.innerHTML =
+      "This is my first project in HTML and CSS. The website is a recreation of a responsive website using only photos as a reference. ";
     recreateLink.innerHTML = "See Project";
     recreateLink.setAttribute(
       "href",
@@ -123,6 +135,7 @@ recreatePhoto.addEventListener("click", () => {
 
     recreateDiv.appendChild(recreateHead);
     recreateDiv.appendChild(recreateText);
+    recreateDiv.appendChild(recreateKey);
     recreateDiv.appendChild(recreateLink);
   }
   recreate = false;
@@ -139,19 +152,24 @@ iomdbPhoto.addEventListener("click", () => {
       "iomdb-mobile"
     ) as HTMLDivElement;
     const iomdbHead: HTMLParagraphElement = document.createElement("p");
+    const iomdbKey: HTMLParagraphElement = document.createElement("p");
     const iomdbText: HTMLSpanElement = document.createElement("span");
     const iomdbLink: HTMLAnchorElement = document.createElement("a");
     iomdbHead.classList.add("clickedHead");
+    iomdbKey.classList.add("clickedKey");
     iomdbText.classList.add("clickedText");
     iomdbLink.classList.add("clickedLink");
     iomdbHead.innerHTML = "School";
-    iomdbText.innerHTML = " iomdb";
+    iomdbKey.innerHTML = "HTML, CSS, Javascript";
+    iomdbText.innerHTML =
+      "This application simulates a movie website, which allows users to browse a list of movies obtained from an API. Users are able to search for a specific movie by title, and the results are presented in a list. The application is easy to use and provides a convenient way to view information about a wide range of movies.";
     iomdbLink.innerHTML = "See Project";
     iomdbLink.setAttribute("href", "https://github.com/miravit/Search-Movies");
     iomdbLink.setAttribute("target", "_blank");
 
     iomdbDiv.appendChild(iomdbHead);
     iomdbDiv.appendChild(iomdbText);
+    iomdbDiv.appendChild(iomdbKey);
     iomdbDiv.appendChild(iomdbLink);
   }
   iomdb = false;
